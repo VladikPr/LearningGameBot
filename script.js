6f9da34a6994f173595b1a1480aaea90ccedb6a3
Vladyslav Pryhara <vladikpr94@hotmail.com>
2020-10-16 14:16:09 +0200

    Lesson6_Changes

--- script.js
+++ script.js
@@ -8,37 +8,38 @@ function isNumber(val){
 // Variables declaration
 //Create a random integer between two numbers
 function getRandomInt(min,max) {
-    return function(){
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max-min)) + min;
-    }
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max-min)) + min;
 }
 
-let targetNumber = getRandomInt(1,100);
-let wishedNumber = targetNumber();
 
 
-function getNumber() {
+function start() {
+    let wishedNumber = getRandomInt(1,100);
 
-let num = prompt("Угадай число от 1 до 100");
+    return function repeatPlay(){
+        let num = prompt("Угадай число от 1 до 100");
 
-    if (num === null) {
-        alert( "Игра окончена")
-    }else if (!isNumber(num*1)){
-        alert("Введи число!");
-        getNumber();
-    }else if (num*1 > wishedNumber) {
-        alert("Загаданное число меньше. Попробуйте снова");
-        getNumber();
-    }else if (num*1 < wishedNumber) {
-        alert("Загаданное число больше. Попробуйте снова");
-        getNumber();
-    }else if ( num*1 == wishedNumber) {
-        alert("Поздравляю, Вы угадали!!!");
+        if (num === null) {
+            alert( "Игра окончена")
+        }else if (!isNumber(num*1)){
+            alert("Введи число!");
+            repeatPlay();
+        }else if (num*1 > wishedNumber) {
+            alert("Загаданное число меньше. Попробуйте снова");
+            repeatPlay();
+        }else if (num*1 < wishedNumber) {
+            alert("Загаданное число больше. Попробуйте снова");
+            repeatPlay();
+        }else if ( num*1 == wishedNumber) {
+            alert("Поздравляю, Вы угадали!!!");
+        }
+
     }
+    
 
 }
 
-getNumber();
+start()();
 
